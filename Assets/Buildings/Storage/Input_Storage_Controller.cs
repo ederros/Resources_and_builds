f6bbadc7e62e3ce9c9ccde00ec27f01998387dfa
ederros <66615068+ederros@users.noreply.github.com>
2023-03-24 10:07:54 +0200

    little improvement

--- Assets/Buildings/Storage/Input_Storage_Controller.cs
+++ Assets/Buildings/Storage/Input_Storage_Controller.cs
@@ -7,11 +7,12 @@ public class Input_Storage_Controller : Storage_Controller
     int ing_ind = 0;// index of ingedients
     protected override void On_Player_In()
     {
-        for(int i = 0;i<my_build.ingredients.Count;i++){
-            ing_ind = (ing_ind+1)%my_build.ingredients.Count;
-            if(!Player_Controller.instance.player_storage.is_mathing(my_build.ingredients[ing_ind].name)) continue;
-            Player_Controller.instance.player_storage.Sub_Res(my_build.ingredients[ing_ind].name,this);
-            break;
-        }
+        if(free_space>0)
+            for(int i = 0;i<my_build.ingredients.Count;i++){
+                ing_ind = (ing_ind+1)%my_build.ingredients.Count;
+                if(!Player_Controller.instance.player_storage.is_mathing(my_build.ingredients[ing_ind].name)) continue;
+                Player_Controller.instance.player_storage.Sub_Res(my_build.ingredients[ing_ind].name,this);
+                break;
+            }
     }
 }
